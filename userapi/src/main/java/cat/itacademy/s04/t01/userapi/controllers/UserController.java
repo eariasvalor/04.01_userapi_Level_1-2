7fdbedb69a3d927eab570cd05a0699a0cdb70822
eariasvalor <eariasvalor@gmail.com>
2025-12-04 18:30:16 +0100

    feat: add endpoint GET / Users

--- userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,11 +1,14 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+@RestController
 public class UserController {
     private static List<User> users;
 
@@ -13,8 +16,9 @@ public class UserController {
         this.users = new ArrayList<>();
     }
 
+    @GetMapping("/users")
     public static List<User> getUsers() {
-        return Collections.unmodifiableList(this.users);
+        return Collections.unmodifiableList(users);
     }
 
 
