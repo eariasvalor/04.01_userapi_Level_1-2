9eb8a82be0cc8517ffb8fc507551493750d934a4
eariasvalor <eariasvalor@gmail.com>
2025-12-04 18:25:58 +0100

    feat: create user class

---
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/entities/User.java
@@ -0,0 +1,36 @@
+package cat.itacademy.s04.t01.userapi.entities;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String name;
+    private String email;
+
+    public User(UUID id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
