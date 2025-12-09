9ac567125e7675ba992ed40c70f1f21e90cd9e37
Esther Arias <eariasvalor@gmail.com>
2025-12-09 10:27:40 +0100

    feat: refactor UserAcceptanceTest

--- userapi/src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserAcceptanceTest.java
+++ userapi/src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserAcceptanceTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -20,7 +22,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 
-@WebMvcTest(UserController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
